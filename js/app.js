373acc779ba1bb26269c9c3c7a8f42cd91e3ac8a
Infant Frontender <newbornfrontender@gmail.com>
2018-10-21 21:54:39 +0300

    Завершил 49 лекцию

--- js/app.js
+++ js/app.js
@@ -18,12 +18,22 @@ new Vue({
         return;
       };
 
-      this.playerHealth -= this.calculateDamage(5, 12);
-      this.checkWin()
+      this.monsterAttacks();
     },
-    specialAttack() {},
+    specialAttack() {
+      this.monsterHealth -= this.calculateDamage(10, 20);
+      if (this.checkWin()) {
+        return;
+      };
+
+      this.monsterAttacks();
+    },
     heal() {},
     giveUp() {},
+    monsterAttacks() {
+      this.playerHealth -= this.calculateDamage(5, 12);
+      this.checkWin();
+    },
     calculateDamage(min, max) {
       return Math.max(Math.floor(Math.random() * max) + 1, min);
     },
