9054b1ca0361ff56b85ed3d462c49311d7025526
Infant Frontender <newbornfrontender@gmail.com>
2018-10-22 18:41:17 +0300

    Инициализировал проект при помощи Vue CLI

---
+++ vue.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  lintOnSave: false,
+  baseUrl: undefined,
+  outputDir: undefined,
+  assetsDir: undefined,
+  runtimeCompiler: undefined,
+  productionSourceMap: undefined,
+  parallel: undefined,
+
+  css: {
+    sourceMap: true
+  }
+};
