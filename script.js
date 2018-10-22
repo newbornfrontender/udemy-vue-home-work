493ad60febcc13ace4d8f17e44138336b0776b68
Infant Frontender <newbornfrontender@gmail.com>
2018-10-22 12:00:37 +0300

    Подготовил файлы к 58 лекции

---
+++ script.js
@@ -0,0 +1,25 @@
+new Vue({
+  data: {
+    title: 'The VueJS Instance',
+    showParagraph: false,
+  },
+  methods: {
+    show() {
+      this.showParagraph = true;
+      this.updateTitle('The VueJS Instance (Updated)');
+    },
+    updateTitle(title) {
+      this.title = title;
+    },
+  },
+  computed: {
+    lowercaseTitle() {
+      return this.title.toLowerCase();
+    },
+  },
+  watch: {
+    title(value) {
+      alert(`Title changed, new value: ${value}`);
+    },
+  },
+}).$mount('#app');
